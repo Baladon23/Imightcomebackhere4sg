ce615e9410690f649fe33c51b511655e183d84f2
Baladon23 <38882702+Baladon23@users.noreply.github.com>
2018-12-09 14:22:35 +0100

    Update and rename way4 to way4.cpp

---
+++ CPP/Fewbooks/way4.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <stdlib.h>
+ 
+using namespace std;
+ 
+int main() {
+    int n, k = 0;
+    cin >> n;
+    string s;
+    getline(cin, s);
+    int y[n];
+ 
+    for (int i = 0;i < n;i++) {
+        char C;
+        do {cin >> C;} while (C != ';');
+        do {cin >> C;} while (C != ';');
+        int c, d;
+        cin >> c >> C >> d;
+        if (c-d <= 2) {
+            k++;
+            y[i] = i + 1;
+        }else {
+            y[i] = -1;
+        }
+    }
+ 
+    cout << k << " ";
+    for (int i = 0;i < n;i++) {
+        if (y[i] != -1) {
+            cout << y[i] << " ";
+        }
+    }
+ 
+    return 0;
+}
